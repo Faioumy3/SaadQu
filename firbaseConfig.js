76f693bd2ea4f1a9988629dc411318a3748e14ae
Mohammed Mahmoud Mohammed Mohammed Elfaioumy <mohamedal-fayoumi84708@azhar.edu.eg>
2025-11-29 19:30:50 +0200

    Update firbaseConfig.js

--- firbaseConfig.js
+++ firbaseConfig.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAfbHhSxXazdJmgraHWm4BtujUGoWcEUQU",
+  authDomain: "saadqu-92cfc.firebaseapp.com",
+  projectId: "saadqu-92cfc",
+  storageBucket: "saadqu-92cfc.firebasestorage.app",
+  messagingSenderId: "143670086980",
+  appId: "1:143670086980:web:81b09b0103966856269b11",
+  measurementId: "G-B1NH1DGEE3"
+};
